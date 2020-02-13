7f8a19bb1041a892b7d14b9effbb55b86b6be931
Johan Peltenburg <j.w.peltenburg@tudelft.nl>
2020-02-13 15:10:36 +0100

    Add GenerateProject trait to generate source files from common Projects

--- src/generator/mod.rs
+++ src/generator/mod.rs
@@ -2,14 +2,23 @@
 //!
 //! The generator module is enabled by the `generator` feature flag.
 
+use std::{error::Error, path::Path};
+
 use crate::{
-    generator::common::{Component, Field, Mode, Port, Record, Type},
+    generator::common::{Component, Field, Mode, Port, Project, Record, Type},
     physical::{BitField, Dir},
     LogicalStream, PhysicalStream, Streamlet,
 };
 
 pub mod common;
 pub mod vhdl;
+
+/// Trait to generate back-end specific source files from the common hardware representation
+/// of a project.
+trait GenerateProject {
+    /// Generate source files from a [common::Project] and save them to [path].
+    fn generate(&self, project: Project, path: &Path) -> Result<(), Box<dyn Error>>;
+}
 
 fn log2ceil(v: usize) -> usize {
     (v as f64).log2().ceil() as usize
