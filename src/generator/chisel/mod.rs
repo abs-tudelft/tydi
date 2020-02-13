60604173384566a91ad9092701ab399676e4ac50
Johan Peltenburg <j.w.peltenburg@tudelft.nl>
2020-02-13 16:57:26 +0100

    Add stub for Chisel back-end

---
+++ src/generator/chisel/mod.rs
@@ -0,0 +1,48 @@
+//! Chisel back-end.
+
+use std::{error::Error, path::Path};
+
+use crate::generator::{common::Project, GenerateProject};
+
+/// Chisel back-end errors.
+#[derive(Debug, Clone, PartialEq)]
+pub enum ChiselError {}
+
+impl std::fmt::Display for ChiselError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, ".")
+    }
+}
+
+impl std::error::Error for ChiselError {}
+
+/// Chisel back-end code generation result
+type ChiselResult = Result<String, ChiselError>;
+
+/// Chisel back-end configuration parameters.
+pub struct ChiselConfig {
+    /// An optional suffix appended to generated files.
+    /// The suffix is added as follows: <filename>.<suffix>.scala
+    gen_suffix: Option<String>,
+}
+
+impl Default for ChiselConfig {
+    fn default() -> Self {
+        ChiselConfig {
+            gen_suffix: Some("gen".to_string()),
+        }
+    }
+}
+
+/// A configurable VHDL back-end entry point.
+#[derive(Default)]
+pub struct ChiselBackEnd {
+    /// Configuration for the VHDL back-end.
+    config: ChiselConfig,
+}
+
+impl GenerateProject for ChiselBackEnd {
+    fn generate(&self, project: &Project, path: &Path) -> Result<(), Box<dyn Error>> {
+        unimplemented!();
+    }
+}
