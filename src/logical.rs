c6442be3df906b609f6e6337a54fe696b468ffdd
Matthijs Brobbel <m1brobbel@gmail.com>
2020-02-19 13:43:57 +0100

    Remove box from stream constructor

--- src/logical.rs
+++ src/logical.rs
@@ -160,7 +160,7 @@ impl Stream {
         synchronicity: Synchronicity,
         complexity: impl Into<Complexity>,
         direction: Direction,
-        user: Option<Box<LogicalStreamType>>,
+        user: Option<LogicalStreamType>,
         keep: bool,
     ) -> Self {
         Stream {
@@ -170,7 +170,7 @@ impl Stream {
             synchronicity,
             complexity: complexity.into(),
             direction,
-            user,
+            user: user.map(Box::new),
             keep,
         }
     }
@@ -528,7 +528,7 @@ impl LogicalStreamType {
                             stream_in.synchronicity,
                             stream_in.complexity.clone(),
                             stream_in.direction,
-                            stream_in.user.clone(),
+                            stream_in.user.clone().map(|stream| *stream),
                             stream_in.keep,
                         )
                         .into(),
