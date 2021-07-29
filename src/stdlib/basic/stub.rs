66be8f415c33b4d5bff081e73f6d6e94842dd355
matthijsr <matthijsr@users.noreply.github.com>
2021-07-29 15:41:05 +0200

    Stub can be source, sink or passthrough.

--- src/stdlib/basic/stub.rs
+++ src/stdlib/basic/stub.rs
@@ -14,96 +14,95 @@ use std::convert::TryFrom;
 
 /// Stub construct, this can be used to prototype a dependency graph
 /// or as a basis for custom components.
-/// * Passes inputs directly to outputs
-/// * Drives defaults where input does not match output
-/// * Drives defaults when no input exists, assumes a Null stream at default complexity as input
-/// * If output is not specified, uses input stream properties.
+/// * If input and output match, passes inputs directly to outputs
+/// * If no input exists, acts as a source and drives defaults values to output.
+/// * If no output exists, acts as a sink.
 #[derive(Clone, Debug)]
-pub struct Stub {
-    streamlet: Streamlet,
-    input_gen: bool,
-    output_gen: bool,
+pub enum Stub {
+    Source(Streamlet),
+    Sink(Streamlet),
+    Passthrough(Streamlet),
 }
 
 impl GenericComponent for Stub {
     fn streamlet(&self) -> &Streamlet {
-        self.streamlet.borrow()
+        match self {
+            Stub::Source(s) => s.borrow(),
+            Stub::Sink(s) => s.borrow(),
+            Stub::Passthrough(s) => s.borrow(),
+        }
     }
 }
 
 impl Stub {
     pub fn try_new(project: &Project, name: Name, op: StreamletHandle) -> Result<Self> {
         let op = project.get_lib(op.lib())?.get_streamlet(op.streamlet())?;
-        let mut input_gen: bool = false;
-        let mut output_gen: bool = false;
+        let mut is_source: bool = false;
+        let mut is_sink: bool = false;
 
-        let stream_in = match op.inputs().next().unwrap().typ() {
-            LogicalType::Stream(s) => s,
-            _ => {
-                log::warn!("No input stream, assuming Null stream.");
-                input_gen = true;
-                Stream::new_basic(LogicalType::Null)
-            }
-        };
+        let stream_in = op.inputs().find_map(|x| match x.typ() {
+            LogicalType::Stream(s) => Some(s),
+            _ => None
+        });
 
-        let stream_out = match op.outputs().next().unwrap().typ() {
-            LogicalType::Stream(s) => s,
-            _ => {
-                log::warn!("No output stream, using input properties instead.");
-                output_gen = true;
-                stream_in.clone()
-            }
-        };
-
-        // TODO: Is there no way to check *just* complexity? Is this already done at an earlier stage?
-        // I'd prefer if this didn't fail on incompatible data types.
-        // if !LogicalType::Stream(stream_in).compatible(&LogicalType::Stream(stream_out)) {
-        //     return Err(Error::ComposerError(format!(
-        //         "The input stream is incompatible with the output stream.",
-        //     )))
-        // }
+        let stream_out = op.outputs().find_map(|x| match x.typ() {
+            LogicalType::Stream(s) => Some(s),
+            _ => None
+        });
 
         let mut ifaces: Vec<Interface> = vec![];
-        ifaces.push(Interface::try_new(
-            "in",
-            Mode::In,
-            stream_in.clone(),
-            if input_gen {
-                Some("Generated, no input was defined.")
-            } else {
-                None
-            },
-        )?);
-        ifaces.push(Interface::try_new(
-            "out",
-            Mode::Out,
-            stream_out.clone(),
-            if output_gen {
-                Some("Generated, no output was defined.")
-            } else {
-                None
-            },
-        )?);
+        if let Some(s) = stream_in {
+            ifaces.push(Interface::try_new("in", Mode::In, s.clone(), None)?);
+        } else {
+            log::info!("Attempting to implement as source.");
+            is_source = true;
+        }
 
-        Ok(Stub {
-            streamlet: Streamlet::from_builder(
-                StreamletKey::try_from(name).unwrap(),
-                UniqueKeyBuilder::new().with_items(ifaces),
-                None,
-            )
-            .unwrap(),
-            input_gen,
-            output_gen,
+        if let Some(s) = stream_out {
+            ifaces.push(Interface::try_new("out", Mode::Out, s.clone(), None)?);
+        } else if !is_source {
+            log::info!("Implementing as sink.");
+            is_sink = true;
+        } else {
+            return Err(Error::ComposerError(format!("No input or output defined.")));
+        }
+
+        let streamlet = Streamlet::from_builder(
+            StreamletKey::try_from(name).unwrap(),
+            UniqueKeyBuilder::new().with_items(ifaces),
+            None,
+        )?;
+
+        Ok(if is_source {
+            Stub::Source(streamlet)
+        } else if is_sink {
+            Stub::Sink(streamlet)
+        } else {
+            Stub::Passthrough(streamlet)
         })
     }
 
     pub fn with_backend(&mut self, name: Name, streamlet_handle: StreamletHandle) -> Result<()> {
-        //self.backend = Option::from(MapStreamBackend { name, streamlet_handle });
-        self.streamlet
-            .attach_implementation(Implementation::Backend(Box::new(StubBackend {
-                name,
-                streamlet_handle,
-            })))?;
+        match self {
+            Stub::Source(s) => {
+                s.attach_implementation(Implementation::Backend(Box::new(SourceStubBackend {
+                    name,
+                    streamlet_handle,
+                })))?
+            }
+            Stub::Sink(s) => {
+                s.attach_implementation(Implementation::Backend(Box::new(SinkStubBackend {
+                    name,
+                    streamlet_handle,
+                })))?
+            }
+            Stub::Passthrough(s) => s.attach_implementation(Implementation::Backend(Box::new(
+                PassthroughStubBackend {
+                    name,
+                    streamlet_handle,
+                },
+            )))?,
+        }
         Ok(())
     }
 
@@ -112,12 +111,22 @@ impl Stub {
     }
 }
 
-pub struct StubBackend {
+pub struct SourceStubBackend {
     name: Name,
     streamlet_handle: StreamletHandle,
 }
 
-impl ImplementationBackend for StubBackend {
+pub struct SinkStubBackend {
+    name: Name,
+    streamlet_handle: StreamletHandle,
+}
+
+pub struct PassthroughStubBackend {
+    name: Name,
+    streamlet_handle: StreamletHandle,
+}
+
+impl ImplementationBackend for SourceStubBackend {
     fn name(&self) -> Name {
         self.name.clone()
     }
@@ -131,50 +140,123 @@ impl ImplementationBackend for StubBackend {
     }
 }
 
-impl GenerateProject for StubBackend {
-    fn generate(&self, project: &Project, path: impl AsRef<std::path::Path>) -> Result<()> {
-        todo!()
+impl ImplementationBackend for SinkStubBackend {
+    fn name(&self) -> Name {
+        self.name.clone()
+    }
+
+    fn streamlet_handle(&self) -> StreamletHandle {
+        self.streamlet_handle.clone()
+    }
+
+    fn connect_action(&self) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+impl ImplementationBackend for PassthroughStubBackend {
+    fn name(&self) -> Name {
+        self.name.clone()
+    }
+
+    fn streamlet_handle(&self) -> StreamletHandle {
+        self.streamlet_handle.clone()
+    }
+
+    fn connect_action(&self) -> Result<()> {
+        unimplemented!()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use std::convert::TryFrom;
-    use std::fs;
 
-    use crate::Name;
+    use crate::design::Library;
+
     use crate::Result;
-    use crate::design::{LibKey, Library};
-    use crate::generator::common::convert::Projectify;
-    use crate::stdlib::tests::composition_test_proj;
+    use crate::{parser, Name};
 
     use super::*;
     use crate::design::Project;
-    use crate::generator::{vhdl::VHDLBackEnd, GenerateProject};
+
+    fn parsed_project() -> Result<Project> {
+        let mut prj = Project::new(Name::try_from("test_project")?);
+        let (_, source_stub) =
+            parser::nom::streamlet("Streamlet source_stub (out_source : out Stream<Bits<1>, d=0>)")
+                .unwrap();
+        let (_, passthrough_stub) = parser::nom::streamlet(
+            "Streamlet passthrough_stub (in_pass : in Stream<Bits<1>, d=0>, out_pass : out Stream<Bits<1>, d=0>)",
+        )
+        .unwrap();
+        let (_, sink_stub) =
+            parser::nom::streamlet("Streamlet sink_stub (in_sink : in Stream<Bits<1>, d=0>)")
+                .unwrap();
+        let lib = Library::try_new(
+            Name::try_from("test_library")?,
+            vec![],
+            vec![source_stub, passthrough_stub, sink_stub],
+        )?;
+        prj.add_lib(lib)?;
+        Ok(prj)
+    }
 
     #[test]
-    fn vhdl_impl() -> Result<()> {
-        let _folder = fs::create_dir_all("output").unwrap();
-
-        let primitives_lib_key = LibKey::try_from("primitives")?;
-        let mut prj = composition_test_proj()?;
-        let vhdl = VHDLBackEnd::default();
-        let stub = Stub::try_new(
+    fn source_stub_interfaces() -> Result<()> {
+        let lib_key = Name::try_from("test_library")?;
+        let prj = parsed_project()?;
+        let source_stub = Stub::try_new(
             &prj,
-             Name::try_from("stub1")?,
-            StreamletHandle{
-                lib: primitives_lib_key.clone(),
-                streamlet: Name::try_from("test_op")?,
+            Name::try_from("source")?,
+            StreamletHandle {
+                lib: lib_key.clone(),
+                streamlet: Name::try_from("source_stub")?,
             },
         )?;
-        prj.get_lib_mut(primitives_lib_key.clone())?.add_streamlet(stub.streamlet().clone())?;
 
-        assert!(vhdl.generate(&prj, "output").is_ok());
+        // prj.get_lib_mut(lib_key.clone())?.add_streamlet(source_stub.streamlet().clone())?;
 
-        // println!(
-        //     "Map interface {:?}",
-        //     vhdl
-        // );
+        // let _folder = fs::create_dir_all("output")?;
+        // let vhdl = VHDLBackEnd::default();
+        // vhdl.generate(&prj, "output")?;
+
+        println!("Stub interface\n {:?}\n", source_stub);
+
+        Ok(())
+    }
+
+    #[test]
+    fn passthrough_stub_interfaces() -> Result<()> {
+        let lib_key = Name::try_from("test_library")?;
+        let prj = parsed_project()?;
+        let stub = Stub::try_new(
+            &prj,
+            Name::try_from("passthrough")?,
+            StreamletHandle {
+                lib: lib_key.clone(),
+                streamlet: Name::try_from("passthrough_stub")?,
+            },
+        )?;
+
+        println!("Stub interface\n {:?}\n", stub);
+
+        Ok(())
+    }
+
+    #[test]
+    fn sink_stub_interfaces() -> Result<()> {
+        let lib_key = Name::try_from("test_library")?;
+        let prj = parsed_project()?;
+        let stub = Stub::try_new(
+            &prj,
+            Name::try_from("sink")?,
+            StreamletHandle {
+                lib: lib_key.clone(),
+                streamlet: Name::try_from("sink_stub")?,
+            },
+        )?;
+
+        println!("Stub interface\n {:?}\n", stub);
 
         Ok(())
     }
